66c6b2cd5f92fe93a807798eb1a286ca7227e4dd
Paul <paul.cabon@protonmail.com>
2022-12-12 21:59:50 +0100

    generator v1

--- generator/generator.py
+++ generator/generator.py
@@ -1,7 +1,17 @@
 import random
 
-def password_generator():
-    return random.randint(1000,9999)
+def get_word():
+    word = "" 
+    for i in range(random.randint(3,7)):
+        word += chr(random.randint(ord("a"),ord("z")))
+    return word
+
+def password_generator(nb_words=3,separator="_"):
+    word = ""
+    for n in range(nb_words):
+        sep = "" if n == nb_words - 1 else separator
+        word += get_word() + sep
+    return word
 
 def main():
     print(password_generator())
--- web/web.py
+++ web/web.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 import streamlit as st
 import sys
 sys.path.insert(0,'../generator')
@@ -6,12 +7,21 @@ from generator import password_generator
 
 def main():
     st.title("Generator de mot de passe breton")
-    size = int(st.number_input("How many words ?"))
+    size = st.number_input(
+        label="How many words ?",
+        min_value=2,
+        max_value=30,
+        step=1,
+        value=3,
+        format="%d"
+    )
+
     separator = st.text_input(
-        "Separator",
+        label="Separator",
+        value="_",
         placeholder="_",
     )
-    st.write(f"Mot de passe : {password_generator(),size,separator}")	
+    st.write(f"Mot de passe : {password_generator(size,separator)}")	
 
 if __name__ == "__main__":
     main()
